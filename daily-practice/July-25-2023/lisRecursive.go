8eb379e9545856391a27c1b13bd1b65b1e879620
ywang096 <ywang096@cable.comcast.com>
2023-07-25 15:04:51 -0400

    Practice

---
+++ daily-practice/July-25-2023/lisRecursive.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{9, 10, 2, 5, 3, 7, 101, 18}
+
+	fmt.Println("000tester: ", lisRecursive(arr))
+}
+
+func lisRecursive(arr []int) []int {
+	arrLen := len(arr)
+
+	dp := make([][]int, arrLen)
+	for arrIndex, arrEle := range arr {
+		dp[arrIndex] = []int{arrEle}
+	}
+
+	var lisHelper func(index int) []int
+	lisHelper = func(index int) []int {
+		tempLis := []int{}
+		for j := index - 1; j >= 0; j-- {
+			if arr[index] > arr[j] {
+				subsequence := lisHelper(j)
+				if len(subsequence) > len(tempLis) {
+					tempLis = subsequence
+				}
+			}
+		}
+		return append(tempLis, arr[index])
+	}
+
+	for i := 1; i < arrLen; i++ {
+		subsequence := lisHelper(i)
+		if len(subsequence) > len(dp[i]) {
+			dp[i] = subsequence	
+		}
+	}
+
+	res := []int{}
+
+	for _, dpEle := range dp {
+		if len(dpEle) > len(res) {
+			res = dpEle
+		}
+	}
+
+	return res
+}
